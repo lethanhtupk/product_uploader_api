542952764498b726374abc5314bc7bde14876dbb
TuLT <tult@rikkeisoft.com>
2021-09-12 22:26:53 +0700

    fix: change the limit of a page

--- product_uploader_api/settings.py
+++ product_uploader_api/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
