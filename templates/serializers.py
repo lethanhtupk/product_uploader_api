c87fc90412251e476af77fdeb722022869041880
TuLT <tult@rikkeisoft.com>
2021-09-15 02:21:58 +0700

    fix: do not delete old data when edit is failed

--- templates/serializers.py
+++ templates/serializers.py
@@ -122,44 +122,48 @@ class TemplateSerializer(serializers.ModelSerializer):
         return template
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.product_title = validated_data.get(
-            'product_title', instance.product_title)
-        instance.description = validated_data.get(
-            'description', instance.description)
+        try:
+            instance.name = validated_data.get('name', instance.name)
+            instance.product_title = validated_data.get(
+                'product_title', instance.product_title)
+            instance.description = validated_data.get(
+                'description', instance.description)
 
-        # TODO: delete old attributes
-        Attribute.objects.filter(template=instance).delete()
-        Variation.objects.filter(template=instance).delete()
+            # TODO: re-create attributes and variations with new data
+            attributes_data = validated_data.pop('attributes')
+            variations_data = validated_data.pop('variations')
 
-        # TODO: re-create attributes and variations with new data
-        attributes_data = validated_data.pop('attributes')
-        variations_data = validated_data.pop('variations')
-
-        for attribute_data in attributes_data:
-            attribute_data = dict(attribute_data)
-            options_data = attribute_data.pop('options')
-            attribute, _ = Attribute.objects.get_or_create(
-                **attribute_data, template=instance)
-            for option_data in options_data:
-                AttributeOption.objects.get_or_create(
-                    attribute=attribute, **dict(option_data))
-
-        for variation_data in variations_data:
-            variation_data = dict(variation_data)
-            attributes_data = variation_data.pop('attributes')
-            variation = Variation.objects.create(
-                template=instance, **variation_data)
             for attribute_data in attributes_data:
                 attribute_data = dict(attribute_data)
-                # TODO: query attribute belong to a specific template
-                attribute = Attribute.objects.get(
-                    name=attribute_data.get('name'), template=instance)
-                # TODO: query attribute option belong to specify attribute
-                attribute_option = AttributeOption.objects.get(
-                    code=attribute_data.get('value'), attribute=attribute)
-                VariationAttribute.objects.create(
-                    variation=variation, name=attribute, value=attribute_option)
+                options_data = attribute_data.pop('options')
+                attribute, _ = Attribute.objects.get_or_create(
+                    **attribute_data, template=instance)
+                for option_data in options_data:
+                    AttributeOption.objects.get_or_create(
+                        attribute=attribute, **dict(option_data))
+
+            for variation_data in variations_data:
+                variation_data = dict(variation_data)
+                attributes_data = variation_data.pop('attributes')
+                variation = Variation.objects.create(
+                    template=instance, **variation_data)
+                for attribute_data in attributes_data:
+                    attribute_data = dict(attribute_data)
+                    # TODO: query attribute belong to a specific template
+                    attribute = Attribute.objects.get(
+                        name=attribute_data.get('name'), template=instance)
+                    # TODO: query attribute option belong to specify attribute
+                    attribute_option = AttributeOption.objects.get(
+                        code=attribute_data.get('value'), attribute=attribute)
+                    VariationAttribute.objects.create(
+                        variation=variation, name=attribute, value=attribute_option)
+
+            # TODO: delete old attributes
+            Attribute.objects.filter(template=instance).delete()
+            Variation.objects.filter(template=instance).delete()
+
+        except Exception as e:
+            print(e)
 
         instance.save()
         return instance
