f2c767856cb2bb23e8ac1ac4dc4a234e84dcbeb8
tult <lethanhtu1551998@gmail.com>
2021-12-03 22:49:27 +0700

    fix: update cross origin

--- product_uploader_api/settings.py
+++ product_uploader_api/settings.py
@@ -150,6 +150,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://product-uploader-2f031.web.app",
+    "https://product-uploader-vsovr.ondigitalocean.app",
 ]
 AUTH_USER_MODEL = 'users.CustomUser'
 
